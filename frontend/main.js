35141dc1bc717a662f7356b22308b00ac70eaffe
buzilaan <andreea.buzila@mail.utoronto.ca>
2019-11-09 15:24:27 -0500

    implemented post request of clicking data every 3s

--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,55 @@
+var threeSecondsClickData = []
+
 function printMouseDown(event) {
-    var textContent =
-    "clientX: " + event.clientX +
-    " - clientY: " + event.clientY;
+    // let textContent =
+    // "clientX: " + event.clientX +
+    // " - clientY: " + event.clientY;
+
+    let downTime = Date.now()
+    let dictDown = {"time": downTime, "clickType": "down"}
+    threeSecondsClickData.push(dictDown)
+    
 }
 
 function printMouseUp(event) {
-    var textContent =
-    "clientX: " + event.clientX +
-    " - clientY: " + event.clientY;
-    alert(textContent)
+    // let textContent =
+    // "clientX: " + event.clientX +
+    // " - clientY: " + event.clientY;
+
+    let upTime = Date.now()
+    let dictUp = {"time": upTime, "clickType": "up"}
+    threeSecondsClickData.push(dictUp)
+    
+}
+
+function sendThreeSeconds() {
+    console.log(threeSecondsClickData)
+    
+
+    const request = new Request("http://localhost:5000/data", {
+        method: 'post',
+        body: JSON.stringify(threeSecondsClickData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    fetch(request).then((res)=> {
+        threeSecondsClickData = []
+    }
+
+    ).catch((error) => { console.log("an error occurred", error)
+
+    threeSecondsClickData = []
+
+})
+
+
+    
+
+
 }
 
 document.addEventListener("mousedown", printMouseDown)
 document.addEventListener("mouseup", printMouseUp)
+window.setInterval(sendThreeSeconds, 3000);
+//myLet = setTimeout(sendThreeSeconds, 3000);
