6456f063ab0df9366bf4ebfaefac8cf8f2f43636
buzilaan <andreea.buzila@mail.utoronto.ca>
2019-11-09 14:13:56 -0500

    Added click location functionality

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,15 @@
-alert("Hello from your Chrome extension!")
+function printMouseDown(event) {
+    var textContent =
+    "clientX: " + event.clientX +
+    " - clientY: " + event.clientY;
+}
+
+function printMouseUp(event) {
+    var textContent =
+    "clientX: " + event.clientX +
+    " - clientY: " + event.clientY;
+    alert(textContent)
+}
+
+document.addEventListener("mousedown", printMouseDown)
+document.addEventListener("mouseup", printMouseUp)
